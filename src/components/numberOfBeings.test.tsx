3094c05337332dffe2a5f53a6efde7bce00d5f9b
judejay <judejay26@gmail.com>
2023-12-17 20:38:18 +0000

    Test: Validate number of beings

--- src/components/numberOfBeings.test.tsx
+++ src/components/numberOfBeings.test.tsx
@@ -35,3 +35,27 @@ test("Number of beings  input field displays value passed in through props", () 
     screen.getByLabelText(/Number of Beings/i);
   expect(inputField.value).toBe("10");
 });
+test("Validation rules pass test", () => {
+  const mockNumberOfBeings = jest.fn();
+  const props = {
+    numberOfBeings: 10,
+    onChangeNumberOfBeings: () => {},
+    validate: mockNumberOfBeings.mockReturnValue([]),
+  };
+  render(<NumberOfBeings {...props} />);
+
+  expect(screen.queryByTestId("error")).not.toBeInTheDocument();
+});
+test("Validation rules fail test", () => {
+  const mockNumberOfBeings = jest.fn();
+  const props = {
+    numberOfBeings: 10,
+    onChangeNumberOfBeings: () => {},
+    validate: mockNumberOfBeings.mockReturnValue([
+      "Must be between more than 10 milliom",
+    ]),
+  };
+  render(<NumberOfBeings {...props} />);
+
+  expect(screen.getByTestId("error")).toBeInTheDocument();
+});
