d2ef81d97f819fd95eb17f388dd275d886d4f993
judejay <judejay26@gmail.com>
2023-12-17 20:45:25 +0000

    Test: Validate reason for sparring

--- src/components/reasonForSparing.test.tsx
+++ src/components/reasonForSparing.test.tsx
@@ -35,3 +35,27 @@ test("Reason for sparing  input field displays value passed in through props", (
     screen.getByLabelText(/Reason for sparing/i);
   expect(inputField.value).toBe("no reason");
 });
+test("Validation rules pass test", () => {
+  const mockValidateReasonForSparing = jest.fn();
+  const props = {
+    reasonForSparing: "no reason",
+    onChangeReasonForSparing: () => {},
+    validate: mockValidateReasonForSparing.mockReturnValue([]),
+  };
+  render(<ReasonForSparing {...props} />);
+
+  expect(screen.queryByTestId("error")).not.toBeInTheDocument();
+});
+test("Validation rules fail test", () => {
+  const mockValidateReasonForSparing = jest.fn();
+  const props = {
+    reasonForSparing: "no reason",
+    onChangeReasonForSparing: () => {},
+    validate: mockValidateReasonForSparing.mockReturnValue([
+      "Must be more than 17 characters",
+    ]),
+  };
+  render(<ReasonForSparing {...props} />);
+
+  expect(screen.getByTestId("error")).toBeInTheDocument();
+});
