7f4202bd8ddc39a9e5da658e9691e3553169e400
judejay <judejay26@gmail.com>
2023-12-17 20:50:05 +0000

    Test: validate what is 4 plus 4

--- src/components/whatIs4Plus4.test.tsx
+++ src/components/whatIs4Plus4.test.tsx
@@ -12,3 +12,26 @@ test("renders label for math check", () => {
   const labelText = screen.getByText("What is 2 + 2?");
   expect(labelText).toBeInTheDocument();
 });
+test("Validation rules pass test", () => {
+  const mockValidateWhatIs4Plus4 = jest.fn();
+  const props = {
+    mathCheck: "2",
+    onChangeMathCheck: () => {},
+    validate: mockValidateWhatIs4Plus4.mockReturnValue([]),
+  };
+  render(<WhatIs4Plus4 {...props} />);
+
+  expect(screen.queryByTestId("error")).not.toBeInTheDocument();
+});
+test("Validation rules fail test", () => {
+  const mockValidateWhatIs4Plus4 = jest.fn();
+
+  const props = {
+    mathCheck: "2",
+    onChangeMathCheck: () => {},
+    validate: mockValidateWhatIs4Plus4.mockReturnValue(["Incorrect answer"]),
+  };
+  render(<WhatIs4Plus4 {...props} />);
+
+  expect(screen.getByTestId("error")).toBeInTheDocument();
+});
